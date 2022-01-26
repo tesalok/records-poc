d576be46cd24cb6651344565c6aac5fca233fec5
Alok <alokk@tesseractlearning.com>
2022-01-26 13:51:02 +0530

    added config8

--- search/development.py
+++ search/development.py
@@ -18,7 +18,7 @@ from search.settings import *
 import dj_database_url, os
 DEBUG = True
 
-ALLOWED_HOSTS = ['poc-search-backend.herokuapp.com']  # allow all
+ALLOWED_HOSTS = ['*']  # allow all
 
 DATABASES = {
     'default':dj_database_url.config(default=os.getenv('DATABASE_URL')),
--- search/settings.py
+++ search/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-^viqw@wvlt857tdy7k2ifaf89^(%0upsf*=udgs8o)t@l@dr*6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
