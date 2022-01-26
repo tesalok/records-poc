5f47076f44673e1cbc9887f51a94c31fd7f3d07e
Alok <alokk@tesseractlearning.com>
2022-01-26 13:44:01 +0530

    added config6

--- search/development.py
+++ search/development.py
@@ -1,16 +1,25 @@
+# from search.settings import *
+# DEBUG = True
+
+# ALLOWED_HOSTS = ['*']  # allow all
+
+# DATABASES = {
+#     'default':
+#         {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'country',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#             'USER': 'postgres',
+#             'PASSWORD': '--'
+#         }
+# }
 from search.settings import *
+import dj_database_url, os
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # allow all
 
 DATABASES = {
-    'default':
-        {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'country',
-            'HOST': 'localhost',
-            'PORT': '5432',
-            'USER': 'postgres',
-            'PASSWORD': '--'
-        }
+    'default':dj_database_url.config(default=os.getenv('DATABASE_URL')),
 }
